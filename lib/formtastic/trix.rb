3266761160b3f0cd3cecf4ba5d2704342fcffb0b
Tim Glen <tim@tagstudios.io>
2017-11-27 17:21:46 -0500

    Moved the assets to app/assets instead of vendor/assets

--- lib/formtastic/trix.rb
+++ lib/formtastic/trix.rb
@@ -2,6 +2,5 @@ require 'formtastic/trix/engine' if defined?(::Rails)
 
 module Formtastic
   module Trix
-    # Your code goes here...
   end
 end
--- lib/formtastic/trix/engine.rb
+++ lib/formtastic/trix/engine.rb
@@ -1,4 +1,5 @@
 module Formtastic
+  # Required for trix.css and trix.js to be discoverable in the asset pipeline
   module Trix
     class Engine < ::Rails::Engine
     end
